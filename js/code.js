8bff88e9a374459b1c58f25f64fd19e8411d68cc
UmayrNordien <umayrnordien@gmail.com>
2022-11-08 11:48:18 +0200

    console and document write versions

--- js/code.js
+++ js/code.js
@@ -1,26 +1,28 @@
 //CONSOLE VERSION
-let userName = prompt('Enter Name:');
-let userSurname = prompt('Enter Surname:');
-let userSalary = prompt('Enter Salary:');
+// let userName = prompt('Enter Name:');
+// let userSurname = prompt('Enter Surname:');
+// let userSalary = prompt('Enter Salary:');
 
 
-let salarySum = eval (userSalary) + 500;
-console.log(`Name:${userName}`);
-console.log(`Surname:${userSurname}`);
-console.log(`Salary:${salarySum}`);
+// let salarySum = eval (userSalary) + 500;
+// console.log(`Name:${userName}`);
+// console.log(`Surname:${userSurname}`);
+// console.log(`Salary:${salarySum}`);
 
 
-//DOCUMENT
-// let firstName = prompt("Type Name: ");
-// let surName = prompt("Type Surname: ");
-// let salary = prompt("Type Salary: ");
-// let bonus = 500;
+// DOCUMENT VERSION
+let firstName = prompt("Enter Name");
+let surName = prompt("Enter Surname");
+let salary = prompt("Enter Salary");
+let bonus = 500;
 
+let finalSalary = eval(`${salary}+${bonus}`);
+document.write(`
+     Name: ${firstName} <br>
+     Surname:  ${surName} <br>
+     Salary: ${finalSalary}
+`)
 
-// document.write("firstName" +firstName);
-// document.write("surName" +surName);
-// let product = eval('${salary}+${bonus}');
-// document.write("<br>salary:R"+product)
 
 
 
