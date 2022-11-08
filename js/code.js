6e744ce02e2e69142aff06bc1b20c1a5aa251aa0
UmayrNordien <umayrnordien@gmail.com>
2022-11-08 11:13:56 +0200

    client salary

---
+++ js/code.js
@@ -0,0 +1,24 @@
+//DOCUMENT
+// let firstName = prompt("Type Name: ");
+// let surName = prompt("Type Surname: ");
+// let salary = prompt("Type Salary: ");
+// let bonus = 500;
+
+
+// document.write("firstName" +firstName);
+// document.write("surName" +surName);
+// let product = eval('${salary}+${bonus}');
+// document.write("<br>salary:R"+product)
+
+
+//CONSOLE
+let userName = prompt('Enter Name:');
+let userSurname = prompt('Enter Surname:');
+let userSalary = prompt('Enter Salary:');
+
+
+let salarySum = eval (userSalary) + 500;
+console.log(`Name:${userName}`);
+console.log(`Surname:${userSurname}`);
+console.log(`Salary:${salarySum}`);
+
